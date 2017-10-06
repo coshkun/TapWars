80eb8e3699820597c054966b3a70dafa6f0454b2
Coskun Caner <coskun@coskun-MacBook-Pro.local>
2017-10-06 10:54:47 +0300

    Xcode project created

---
+++ TapWars/TapWars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapWars
+//
+//  Created by coskun on 6.10.2017.
+//  Copyright © 2017 coskun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
